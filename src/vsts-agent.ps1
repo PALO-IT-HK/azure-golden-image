ba0b818a7e0acd8d5ecd8a7a604b18618fe61c02
Beda Tse <beda@coeus.hk>
2019-07-04 00:49:41 +0800

    remove winRM block

--- src/vsts-agent.ps1
+++ src/vsts-agent.ps1
@@ -39,36 +39,5 @@ Configuration VstsAgentConfig {
             Path = "$packagePath"
             Destination = "$installPath"
         }
-
-        # Script EnableWinRM {
-        #     SetScript = {
-        #         $DnsName = "localhost"
-
-        #         $Cert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -DnsName $DnsName -Verbose
-        #         $Password = ConvertTo-SecureString -String $DnsName -Force -AsPlainText -Verbose
-
-        #         Export-Certificate -Cert $Cert -FilePath .\$DnsName.cer -Verbose
-        #         Export-PfxCertificate -Cert $Cert -FilePath .\$DnsName.pfx -Password $Password -Verbose
-
-        #         $CertThumbprint = $Cert.Thumbprint
-
-        #         Enable-PSRemoting -Force -Verbose
-        #         Set-Item WSMan:\localhost\Client\TrustedHosts * -Force -Verbose
-        #         New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $CertThumbprint -Force -Verbose
-        #         New-NetFirewallRule -DisplayName "Windows Remote Management (HTTPS-In)" -Name "WinRMHTTPSIn" -Profile Any -LocalPort 5986 -Protocol TCP -Verbose
-                
-        #         # TODO: Move restart service outside the script to have it governed by DSC
-        #         Restart-Service WinRM -Verbose
-        #     }
-        #     TestScript = {
-        #         return $true
-        #       # Test-Path "C:\TempFolder\TestFile.txt"
-        #     }
-        #     GetScript = {
-        #         @{
-        #             Result = (Get-Content C:\TempFolder\TestFile.txt)
-        #         }
-        #     }
-        # }
     }
 }
